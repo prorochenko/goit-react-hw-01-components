89f3a7fb7e53b8cb57fb9bed1fb8da211b692d85
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-10-24 22:31:36 +0300

    updated 3 and 4

--- src/components/Statistics/Statistic.jsx
+++ src/components/Statistics/Statistic.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import './statistic.css';
 
 export default function Statistic(props) {
-  const { id, label, percentage } = props;
+  const { label, percentage } = props;
   const randomColor =
     '#' + (((1 << 24) * Math.random()) | 0).toString(16) + '35';
 
